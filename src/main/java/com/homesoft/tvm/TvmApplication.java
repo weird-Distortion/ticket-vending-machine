2b213e6957a26853d57a98b8912d90e89dea08f7
weird-Distortion <ilyalilison@gmail.com>
2019-01-07 07:57:39 +0100

    Initial commit

---
+++ src/main/java/com/homesoft/tvm/TvmApplication.java
@@ -0,0 +1,14 @@
+package com.homesoft.tvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvmApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/homesoft/tvm/TvmApplicationTests.java
@@ -0,0 +1,17 @@
+package com.homesoft.tvm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TvmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
